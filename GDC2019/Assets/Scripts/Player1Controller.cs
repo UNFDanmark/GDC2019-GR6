752e5e495989915c8670ed32cc4d0c4dbd670fb0
Prog <teknisk@game.unf.dk>
2019-07-26 22:27:44 +0200

    End of day 1 programmering

--- GDC2019/Assets/Scripts/Player1Controller.cs
+++ GDC2019/Assets/Scripts/Player1Controller.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player1Controller : MonoBehaviour
 {
+    public float MovementSpeed = 2f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("Test");
+            transform.Translate(new Vector3(0, 0, MovementSpeed));
+        }
+        /*else*/ if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -MovementSpeed));
+        }
+        /*else*/ if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-MovementSpeed, 0, 0));
+        }
+        /*else*/ if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(new Vector3(MovementSpeed, 0, 0));
+        }
     }
 }
