eadab783adac101710f8d1d1080c3deb78b3745e
Prog <teknisk@game.unf.dk>
2019-07-30 17:54:26 +0200

    Fixed gameBreaking bug

--- GDC2019/Assets/Scripts/GameManagerScript.cs
+++ GDC2019/Assets/Scripts/GameManagerScript.cs
@@ -9,11 +9,39 @@ public class GameManagerScript : MonoBehaviour
     public bool HasGameStarted = false;
     public Text OptimoWinText;
     public Text PessimoWinText;
+    public Text OptimoScoreText;
+    public Text PessimoScoreText;
     public int mapSelected;
+    public GameObject canvas;
+
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 0;
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+        Text[] textArray = canvas.GetComponentsInChildren<Text>();
+        foreach (Text scoreText in textArray)
+        {
+            if (scoreText.name == "PessimoScoreText")
+            {
+                PessimoScoreText = scoreText;
+            }
+            else if (scoreText.name == "OptimoScoreText")
+            {
+                OptimoScoreText = scoreText;
+            }
+            else if (scoreText.name == "PessimoWinText")
+            {
+                PessimoWinText = scoreText;
+            }
+            else if (scoreText.name == "OptimoWinText")
+            {
+                OptimoWinText = scoreText;
+            }
+
+        }
+        OptimoScoreText.enabled = true;
+        PessimoScoreText.enabled = true;
     }
 
     // Update is called once per frame
